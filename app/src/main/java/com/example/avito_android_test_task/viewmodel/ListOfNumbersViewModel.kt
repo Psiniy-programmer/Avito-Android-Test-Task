9bf491caf45b19a85355899cb1db0a8f5464080b
psinka <psiniy-programmer@yandex.ru>
2021-01-04 00:33:14 +0300

    1.1 change update time

--- app/src/main/java/com/example/avito_android_test_task/viewmodel/ListOfNumbersViewModel.kt
+++ app/src/main/java/com/example/avito_android_test_task/viewmodel/ListOfNumbersViewModel.kt
@@ -30,7 +30,7 @@ class ListOfNumbersViewModel : ViewModel() {
         viewModelScope.launch {
            while(true) {
                newPos = list.addNumber()
-               delay(1000)
+               delay(5000)
            }
         }
     }
